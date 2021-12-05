cc446ad0c33566cde12ae0e2a69d1602d77caf74
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-12-05 13:18:46 +0300

    Added aggregation on the database side

--- spark_task_3/src/task.py
+++ spark_task_3/src/task.py
@@ -17,7 +17,15 @@ df_age = df_bank \
         .agg(f.count("*").alias("count")) \
         .orderBy("age")
 
+print("Execution plan (aggregation on the Spark side):")
 df_age.explain()
 
+# Doing aggregation on the database side
+pushdown_query = f"(SELECT age, COUNT(*) as count from {TARGET_TABLE} GROUP BY age ORDER BY age) sql_agg"
+df_age_sql = spark.read.jdbc(url=url, table=pushdown_query, properties=connectionProperties)
 
+print("Execution plan (aggregation on the database side):")
+df_age_sql.explain()
+
+# Writing data to a parquet file
 df_age.repartition(1).write.mode("overwrite").format("parquet").save(OUTPUT_FILE)
